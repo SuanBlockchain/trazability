4785ef63cc55b7db2e15dc5f0f8f815f56dda61c
larestrepo <larestrepomu@gmail.com>
2025-02-03 13:14:19 -0500

    bucket again

--- lambda/app.py
+++ lambda/app.py
@@ -5,5 +5,5 @@ def lambda_handler(event, context):
     # database_url = os.getenv("DATABASE_URL")
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "Database populated!", "db_url": "database_url"})
+        "body": json.dumps({"message": "Database populated!", "db_url": "database_url"}) # change to database_url
     }
