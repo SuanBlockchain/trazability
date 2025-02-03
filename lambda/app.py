f8fd485b06f4692cd85f8479f796869b3be453c1
larestrepo <larestrepomu@gmail.com>
2025-02-03 11:01:33 -0500

    First commit

---
+++ lambda/app.py
@@ -0,0 +1,9 @@
+import os
+import json
+
+def lambda_handler(event, context):
+    # database_url = os.getenv("DATABASE_URL")
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"message": "Database populated!", "db_url": "database_url"})
+    }
