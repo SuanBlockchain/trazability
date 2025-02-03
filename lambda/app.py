587bbad8a083e0ff59c6f0d26f32c191479f62b7
larestrepo <larestrepomu@gmail.com>
2025-02-03 16:16:07 -0500

    logging events

--- lambda/app.py
+++ lambda/app.py
@@ -1,8 +1,13 @@
 import json
+import logging
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
+    logger.info(f"Received event: {event}")
     try:
         body = json.loads(event.get("body", "{}"))
+
         return {
             "statusCode": 200,
             "body": json.dumps({"message": "Database populated!", "received_data": body})
