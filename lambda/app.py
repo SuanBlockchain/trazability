ae51d52647101ac69238b3ddc829e60b6592bc7b
larestrepo <larestrepomu@gmail.com>
2025-02-04 21:20:32 -0500

    secrets config using ssm

--- lambda/app.py
+++ lambda/app.py
@@ -2,10 +2,34 @@ from datetime import date
 import time
 import json
 import logging
-import requests
+import os
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+import requests
+from requests.structures import CaseInsensitiveDict
+
+# Initialize SSM client
+ssm = boto3.client("ssm", region_name="us-east-2")
+
+def get_ssm_parameter(name, with_decryption=True):
+    """Retrieve parameter from AWS SSM."""
+    response = ssm.get_parameter(Name=name, WithDecryption=with_decryption)
+    return response["Parameter"]["Value"]
+
+def kobo_api(URL, params= {}):
+    headers = CaseInsensitiveDict()
+    kobo_token = get_ssm_parameter("/myapp/kobo_token")
+    # logger.info(f"Kobo token: {kobo_token}")
+    # kobo_token = os.getenv("kobo_token")
+    headers["Authorization"] = "Token " + str(kobo_token)
+
+    resp = requests.get(URL, headers=headers, params=params)
+    rawResult = resp
+    return rawResult
+
 def lambda_handler(event, context):
     TODAY = date.fromtimestamp(time.time())
     BASE_URL = "https://kf.kobotoolbox.org/api/v2/assets/"
@@ -13,6 +37,9 @@ def lambda_handler(event, context):
         'format': 'json'
     }
     try:
+        rawResult = kobo_api(BASE_URL, params)
+        rawResult = json.loads(rawResult.content.decode('utf-8'))
+        logging.info(f"Raw result: {rawResult}")
         body = json.loads(event.get("body", "{}"))
 
         return {
--- upload_secrets.sh
+++ upload_secrets.sh
@@ -4,9 +4,6 @@
 export $(grep -v '^#' .env | xargs)
 
 # Upload parameters to AWS SSM
-aws ssm put-parameter --name "/myapp/db_host" --value "$DB_HOST" --type "SecureString" --overwrite
-aws ssm put-parameter --name "/myapp/db_user" --value "$DB_USER" --type "SecureString" --overwrite
-aws ssm put-parameter --name "/myapp/db_password" --value "$DB_PASSWORD" --type "SecureString" --overwrite
-aws ssm put-parameter --name "/myapp/api_key" --value "$API_KEY" --type "SecureString" --overwrite
+aws ssm put-parameter --name "/myapp/kobo_token" --value "$KOBO_TOKEN" --type "SecureString" --overwrite
 
 echo "✅ All parameters uploaded to SSM!"
